b54628152d04e6569c2062b9e36983b1faa3f5d9
torellicode <torellicode@gmail.com>
2024-09-02 00:58:38 -0400

    Created full_navigation that uses navigation and vendor/admin navagation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def company_name
+    "ShoplyStore"
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.getElementById('user-menu-button').addEventListener('click', function () {
+  const dropdown = document.getElementById('user-dropdown');
+  dropdown.classList.toggle('hidden');
+});
